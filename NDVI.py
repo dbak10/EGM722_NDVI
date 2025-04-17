ff91e916cb14e840d34b91db3af56bc04ce00808
dbak10 <dbak10d@warpmail.net>
2025-04-17 15:07:22 +0100

    establish aoi for use in sentinelsat image search
    correct geopy import statement, correct point call typo. Correction to input statement as needs eastings, northings not an os ref. need to look at package os-grid-reference. set aoi as geojson for input in sentinelsat image finder.

--- NDVI.py
+++ NDVI.py
@@ -5,7 +5,7 @@ import rasterio.merge       #import module to combine rasters
 import shapely              #import shapely module to work with geometries
 import pandas as pd         #import pandas module
 from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
-import geopy
+from geopy import Point
 from geopy.distance import distance
 import pyproj
 from shapely.geometry import Polygon
@@ -18,7 +18,7 @@ start_date=input()        #set start date for image search user imput?
 print("enter end date in format yyyymmdd")
 end_date=input()          #set end date for image search user input?
 
-corner_grid=input("enter OS 1km four figure corner grid reference e.g. 44000 10000").split()     #input of corner grid, split into list
+corner_grid=input("enter easting, northings for an OS 1km square e.g. 604000 279000").split()     #input of corner grid, split into list
 corner_grid_x= int(corner_grid[0]) #easting of corner grid
 corner_grid_y= int(corner_grid[1]) #northing of corner grid
 
@@ -28,18 +28,20 @@ corner_grid_lon_lat = pyproj.transform(crs_OS, crs_wgs84, corner_grid_x, corner_
 
 
 #create square polygon off corner grid reference
-p1= point(corner_grid_lon_lat[1], corner_grid_lon_lat[0])      #set corner point in lat lon (reverse of transform!)
-p2= distance(kilometers=1).destination((p1), bearing=0)  #set next corner location 1km along
-p3 = distance(kilometers=1).destination((p2), bearing=90)   #set next corner location 1km along
-p4 = distance(kilometers=1).destination((p3), bearing=180)   #set next corner location 1km along
+p1= Point(corner_grid_lon_lat[1], corner_grid_lon_lat[0])      #set corner point in lat lon (reverse of transform!)
+p2= distance(kilometers=1).destination((p1), bearing=0)  #set next corner location 1km North
+p3 = distance(kilometers=1).destination((p2), bearing=90)   #set next corner location 1km East
+p4 = distance(kilometers=1).destination((p3), bearing=180)   #set next corner location 1km South
 p0 = p1     #set a p0 to close the polygon with the same coordinates as the start point
-points = [(p.longitude, p.latitude) for p in [p1,p2,p3,p4, p0]]  #convert points to lat long explicit to create polygon
+points = [(p.longitude, p.latitude) for p in [p1,p2,p3,p4, p0]]  #convert points to lat lon explicit to create polygon
 km_square = Polygon(points)                         #create polygon using the 4 corner points
 
+km_square_gdf=gpd.GeoDataFrame(geometry=[km_square], crs="EPSG:4326") #make polygon a geodataframe with crs
+aoi = km_square_gdf.to_json()
 
 
 #search by polygon, time, and SciHub query keywords
-footprint = geojson_to_wkt(read_geojson(km_square))
+footprint = geojson_to_wkt(aoi)
 products = api.query(footprint,
                      date=(start_date, end_date), #start and end dates from user input variables
                      platformname='Sentinel-2',   #limit to sentinel 2 multispectral sensor
