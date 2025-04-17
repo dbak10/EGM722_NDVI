95e0dc38a0a784c81259d56ff292a305179234f7
dbak10 <dbak10d@warpmail.net>
2025-04-17 13:05:39 +0100

    Create 1km polygon
    code to create 1km polygon from input of corner grid ref using pyproj and geopy. now need to convert shapely polygon into useable input for sentinelsat image search

--- NDVI.py
+++ NDVI.py
@@ -1,23 +1,45 @@
 import os              #import os module to access files and folders
-# import earthaccess      #import eartaccess module to access satellite data
 import geopandas as gpd     #import geopandas to work with spatial data
 import rasterio as rio      #import rasterio to work with raster data
 import rasterio.merge       #import module to combine rasters
 import shapely              #import shapely module to work with geometries
 import pandas as pd         #import pandas module
 from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
-
-
+import geopy
+from geopy.distance import distance
+import pyproj
+from shapely.geometry import Polygon
+import matplotlib as plt
 
 
 api = SentinelAPI(None, None, 'https://apihub.copernicus.eu/apihub/')   #sentinelsat login access
-start_date=(input())         #set start date for image search user imput?
-end_date= (input())          #set end date for image search user input?
-corner_ref=                     #create 1km aoi from corner grid reference
+print("enter start date in format yyyymmdd")
+start_date=input()        #set start date for image search user imput?
+print("enter end date in format yyyymmdd")
+end_date=input()          #set end date for image search user input?
+
+corner_grid=input("enter OS 1km four figure corner grid reference e.g. 44000 10000").split()     #input of corner grid, split into list
+corner_grid_x= int(corner_grid[0]) #easting of corner grid
+corner_grid_y= int(corner_grid[1]) #northing of corner grid
+
+crs_OS = pyproj.Proj(init='EPSG:27700') #set variable with projected OS ctm
+crs_wgs84 = pyproj.Proj(init='EPSG:4326')  #set variale with lat long ctm
+corner_grid_lon_lat = pyproj.transform(crs_OS, crs_wgs84, corner_grid_x, corner_grid_y) #convert corner grid inputs to long lat
 
 
-# search by polygon, time, and SciHub query keywords
-footprint = geojson_to_wkt(read_geojson('/path/to/map.geojson'))
+#create square polygon off corner grid reference
+p1= point(corner_grid_lon_lat[1], corner_grid_lon_lat[0])      #set corner point in lat lon (reverse of transform!)
+p2= distance(kilometers=1).destination((p1), bearing=0)  #set next corner location 1km along
+p3 = distance(kilometers=1).destination((p2), bearing=90)   #set next corner location 1km along
+p4 = distance(kilometers=1).destination((p3), bearing=180)   #set next corner location 1km along
+p0 = p1     #set a p0 to close the polygon with the same coordinates as the start point
+points = [(p.longitude, p.latitude) for p in [p1,p2,p3,p4, p0]]  #convert points to lat long explicit to create polygon
+km_square = Polygon(points)                         #create polygon using the 4 corner points
+
+
+
+#search by polygon, time, and SciHub query keywords
+footprint = geojson_to_wkt(read_geojson(km_square))
 products = api.query(footprint,
                      date=(start_date, end_date), #start and end dates from user input variables
                      platformname='Sentinel-2',   #limit to sentinel 2 multispectral sensor
@@ -37,6 +59,6 @@ api.to_geodataframe(products)
 
 # Get basic information about the product: its title, file size, MD5 sum, date, footprint and
 # its download url
-api.get_product_odata(<product_id>)
+#api.get_product_odata(<product_id>)
 
 
